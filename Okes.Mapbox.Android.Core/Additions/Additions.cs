c45b8279a43d4bc375528630a74c720c00665f28
Jacques <Jacquesokes@gmail.com>
2020-06-09 12:26:16 +0200

    Add project files.

---
+++ Okes.Mapbox.Android.Core/Additions/Additions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Com.Mapbox.Android.Core
+{
+    public sealed partial class FileUtils
+    {
+        public partial class LastModifiedComparator
+        {
+
+            public int Compare(Java.Lang.Object a, Java.Lang.Object b)
+            {
+                return Compare(a as Java.IO.File, b as Java.IO.File);
+            }
+        }
+    }
+
+}
---
+++ Okes.Mapbox.SDK.Geojson/Additions/Additions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Com.Mapbox.Geojson
+{
+    public partial class BoundingBoxTypeAdapter
+    {
+        //public virtual unsafe Com.Mapbox.Geojson.BoundingBox Read(Java.Lang.Object a)
+        //{
+        //    return Read(a as GoogleGson.Stream.JsonReader);
+        //}
+
+        // public unsafe void Write(GoogleGson.Stream.JsonWriter a, Com.Mapbox.Geojson.BoundingBox b)
+        //{
+        //    Write(a as GoogleGson.Stream.JsonWriter, b as Com.Mapbox.Geojson.BoundingBox);
+        //}
+
+    }
+
+}
